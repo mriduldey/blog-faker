d7bf72fba0dd1282bdba28fbdf3b513b02326410
unknown <mriduldey94@gmail.com>
2021-08-07 14:56:02 +0530

    authors page header added for mobile screen

--- src/pages/PostsPage.js
+++ src/pages/PostsPage.js
@@ -66,7 +66,8 @@ const PostsPage = ({ match }) => {
     <Container className="text-white">
       <Row>
         <Col xs={12} lg={3} xl={4} className="mb-5 posts-by">
-          <div className="post-by-wrapper text-center position-fixed d-none d-sm-none d-md-none d-lg-block pt-5 mt-5">
+          <h1 className="d-block d-lg-none">Posts by {user.username}</h1>
+          <div className="post-by-wrapper text-center position-fixed d-none d-lg-block pt-5 mt-5">
             <h1>Posts by</h1>
             {renderUser()}
           </div>
